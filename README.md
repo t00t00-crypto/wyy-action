34df846e398c7a0e41fb8c5b464be5a3f1873df9
Liona <lionacc@163.com>
2021-01-02 19:59:20 +0800

    优化了代码结构,添加了微信通知功能

--- README.md
+++ README.md
@@ -1,33 +1,58 @@
-# 网易云音乐自动签到 + 刷歌
-> 网易云音乐自动登录签到 + 刷歌 310 首
+![](https://i0.hdslb.com/bfs/album/250f77dc6386e0cb9ffc93e9836f96c07c73e701.png)
 
-## Github Actions 部署指南
+# 网易云音乐自动签到 + 刷歌 310首
 
-### 一、Fork 此仓库
+##  Github Actions 部署指南
+
+- [网易云音乐自动签到 + 刷歌 310首](#网易云音乐自动签到--刷歌-310首)
+  - [Github Actions 部署指南](#github-actions-部署指南)
+  - [一、Fork 此仓库](#一fork-此仓库)
+  - [二、设置账号、密码以及微信通知（可选）](#二设置账号密码以及微信通知可选)
+    - [可选择设置Server酱通知](#可选择设置server酱通知)
+  - [三、启用 Action](#三启用-action)
+  - [四、查看运行结果](#四查看运行结果)
+  - [注意事项](#注意事项)
+
+## 一、Fork 此仓库
+
 ![image-20200727142541791](https://i.loli.net/2020/07/27/jK5H8FLvt7aBeYX.png)
 
 
 
-### 二、设置账号密码
-添加名为 **USER**、**PWD** 的变量，值分别为 **账号（仅支持手机号）**、**密码 **
+## 二、设置账号、密码以及微信通知（可选）
 
-> Settings-->Secrets-->New secret
+![](https://i0.hdslb.com/bfs/album/eb3958ad1c3da6528091c63f602789cbe114b5dd.png)
 
-支持多账号，账号之间与密码之间用 ***#*** 分隔，账号与密码的个数要对应
+添加以下变量
 
-示例：**USER:13800000000#13800000001**，**PWD:cxkjntm#jntmcxk**
-![image-20200727142753175](https://i.loli.net/2020/07/27/xjri3p4qdchaf2G.png)
+| 名称  |      | 值                                                                                                                         |
+| ----- | ---- | -------------------------------------------------------------------------------------------------------------------------- |
+| PHONE | 必填 | 用户名，多个用户名之间请用**#** 隔开，账号与密码的要对应 `示例`：**USER:13800000000#13800000001**                          |
+| PWD   | 必填 | 密码，多个密码之间请用**#**隔开，账号与密码的要对应，密码中不能包含**#**，有的话请提前修改 `示例`：**PWD:cxkjntm#jntmcxk** |
+| SCK   | 可选 | Server 酱的SCK                                                                                                             |
 
-### 三、启用 Action
-1. 点击 ***Actions***，再点击 **I understand my workflows, go ahead and enable them**
+### 可选择设置Server酱通知
 
-   ![](https://i.loli.net/2020/07/27/pyQmdMHrOIz4x2f.png)
+> 设置Server酱
+> Server 酱的KEY获取地址
+>
+> http://sc.ftqq.com/3.version
+> ![](https://i0.hdslb.com/bfs/album/74882a3398ba9fc8e6ebbf45c2e165188727d8f7.png)
+>
+> 添加密钥 SCK
 
-2. 点击左侧的 ***Star***
 
-   ![image-20200727142617807](https://i.loli.net/2020/07/27/3cXnHYIbOxfQDZh.png)
 
-### 四、查看运行结果
+## 三、启用 Action
+1. ### 点击 ***Actions***，再点击 **I understand my workflows, go ahead and enable them**
+
+   ![](https://i0.hdslb.com/bfs/album/da43935eda722fabe97f66011bd8ae27f147cc07.png)
+
+2. ### 点击右侧的 ***Star***
+
+   ![](https://i0.hdslb.com/bfs/album/f2d683b011284293fe2f56da22d205148392d764.png)
+
+## 四、查看运行结果
 > Actions --> 签到 --> build
 >
 > 能看到如下图所示，表示成功
@@ -36,8 +61,8 @@
 
 ## 注意事项
 
-1. 每天运行两次，在上午 6 点和晚上 22 点。
+1. ### 每天运行两次，在上午 8 点和晚上 20 点。
 
-2. 可以通过 ***Star*** 手动启动一次。
+2. ### 可以通过 ***Star*** 手动启动一次。
 
    ![image-20200727142617807](https://i.loli.net/2020/07/27/87oQeLJOlZvU3Ep.png)
--- checkin.py
+++ checkin.py
@@ -1,108 +1,245 @@
-import requests,base64,json,hashlib
+import requests
+import base64
+import json
+import sys
+import hashlib
 from Crypto.Cipher import AES
-def encrypt(key, text):
-    cryptor = AES.new(key.encode('utf8'), AES.MODE_CBC, b'0102030405060708')
-    length = 16                    
-    count = len(text.encode('utf-8'))     
-    if (count % length != 0):
-        add = length - (count % length)
-    else:
-        add = 16             
-    pad = chr(add)
-    text1 = text + (pad * add)    
-    ciphertext = cryptor.encrypt(text1.encode('utf8'))          
-    cryptedStr = str(base64.b64encode(ciphertext),encoding='utf-8')
-    return cryptedStr
-def md5(str):
-    hl = hashlib.md5()
-    hl.update(str.encode(encoding='utf-8'))
-    return hl.hexdigest()
-def protect(text):
-    return {"params":encrypt('TA3YiYCfY2dDJQgg',encrypt('0CoJUm6Qyw8W8jud',text)),"encSecKey":"84ca47bca10bad09a6b04c5c927ef077d9b9f1e37098aa3eac6ea70eb59df0aa28b691b7e75e4f1f9831754919ea784c8f74fbfadf2898b0be17849fd656060162857830e241aba44991601f137624094c114ea8d17bce815b0cd4e5b8e2fbaba978c6d1d14dc3d1faf852bdd28818031ccdaaa13a6018e1024e2aae98844210"}
+import logging
+import coloredlogs
 
 
-s=requests.Session()
-header={}
-url="https://music.163.com/weapi/login/cellphone"
-url2="https://music.163.com/weapi/point/dailyTask"
-url3="https://music.163.com/weapi/v1/discovery/recommend/resource"
-logindata={
-    "phone":input(),
-    "countrycode":"86",
-    "password":md5(input()),
-    "rememberLogin":"true",
-}
-headers = {
-        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-        "Referer" : "http://music.163.com/",
-        "Accept-Encoding" : "gzip, deflate",
+class AutoLogin:
+    """
+    网易云自动签到
+    """
+    def __init__(self, username="", password="", SCK=""):
+        self.logger = logging.getLogger(__name__)
+        coloredlogs.install(loggger=self.logger, level="INFO")
+        self.username = username
+        self.password = password
+        self.SCK = SCK
+        self.LOG_INFO = ""
+
+    # NOTE 输出调试信息
+    def log(self, content, status_code=3):
+        if status_code == 1:
+            self.logger.warning(str(content))
+        elif status_code == 2:
+            self.logger.error(str(content))
+        else:
+            self.logger.info(str(content))
+        self.LOG_INFO = self.LOG_INFO + str(content) + '\n\n'
+
+    # NOTE 加密
+    def encrypt(self, key, text):
+        cryptor = AES.new(key.encode('utf8'), AES.MODE_CBC,
+                          b'0102030405060708')
+        length = 16
+        count = len(text.encode('utf-8'))
+        if (count % length != 0):
+            add = length - (count % length)
+        else:
+            add = 16
+        pad = chr(add)
+        text1 = text + (pad * add)
+        ciphertext = cryptor.encrypt(text1.encode('utf8'))
+        cryptedStr = str(base64.b64encode(ciphertext), encoding='utf-8')
+        return cryptedStr
+
+    # NOTE MD5
+    def md5(self, str):
+        hl = hashlib.md5()
+        hl.update(str.encode(encoding='utf-8'))
+        return hl.hexdigest()
+
+    # NOTE 保存密文
+    def protect(self, text):
+        return {
+            "params":
+            self.encrypt('TA3YiYCfY2dDJQgg',
+                         self.encrypt('0CoJUm6Qyw8W8jud', text)),
+            "encSecKey":
+            "84ca47bca10bad09a6b04c5c927ef077d9b9f1e37098aa3eac6ea70eb59df0aa28b691b7e75e4f1f9831754919ea784c8f74fbfadf2898b0be17849fd656060162857830e241aba44991601f137624094c114ea8d17bce815b0cd4e5b8e2fbaba978c6d1d14dc3d1faf852bdd28818031ccdaaa13a6018e1024e2aae98844210"
         }
-headers2 = {
-        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-        "Referer" : "http://music.163.com/",
-        "Accept-Encoding" : "gzip, deflate",
-        "Cookie":"os=pc; osver=Microsoft-Windows-10-Professional-build-10586-64bit; appver=2.0.3.131777; channel=netease; __remember_me=true;"
+
+    # NOTE 发送提示信息
+    def send_msg(self, title="网易云音乐签到信息", msg="", SCK=""):
+        if SCK == "":
+            pass
+        else:
+            data = {"text": title, "desp": msg}
+            res = requests.post(url='https://sc.ftqq.com/' + SCK + ".send",
+                                data=data)
+            if res.status_code == 200:
+                self.log('> 发送通知成功')
+            else:
+                self.log('> 发送通知失败', status_code=2)
+
+    # NOTE 处理帐号密码
+    def deal_username_password(self):
+        """
+        处理输入数据
+        """
+        username_list = self.username.split("#")
+        password_list = self.password.split("#")
+        # OUTPUT 调试使用
+        # self.log(username_list)
+        # self.log(password_list)
+        if (len(username_list) != len(password_list)):
+            self.log("> 帐号密码数量不匹配，密码中包含间隔符#,如果密码中包含#请更改密码，或者手动修改脚本",
+                     status_code=2)
+
+        for username, password in zip(username_list, password_list):
+            self.log("> 帐号 {} 开始执行".format(username))
+            try:
+                self.start(username=username, password=password)
+                self.log("> 帐号 {} 执行完毕".format(username))
+            except AttributeError:
+                self.log("> 帐号 {} 执行出现问题".format(username))
+                pass
+            self.log("==")
+        self.send_msg(msg=self.LOG_INFO, SCK=self.SCK)
+
+    # NOTE 主函数
+    # INFO server_SCK 为server酱的KEY
+    def start(self, username="", password=""):
+        s = requests.Session()
+        header = {}
+        url = "https://music.163.com/weapi/login/cellphone"
+        url2 = "https://music.163.com/weapi/point/dailyTask"
+        url3 = "https://music.163.com/weapi/v1/discovery/recommend/resource"
+        logindata = {
+            "phone": username,
+            "countrycode": "86",
+            "password": self.md5(password),
+            "rememberLogin": "true",
+        }
+        headers = {
+            'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+            "Referer": "http://music.163.com/",
+            "Accept-Encoding": "gzip, deflate",
+        }
+        headers2 = {
+            'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+            "Referer":
+            "http://music.163.com/",
+            "Accept-Encoding":
+            "gzip, deflate",
+            "Cookie":
+            "os=pc; osver=Microsoft-Windows-10-Professional-build-10586-64bit; appver=2.0.3.131777; channel=netease; __remember_me=true;"
         }
 
-res=s.post(url=url,data=protect(json.dumps(logindata)),headers=headers2)
-tempcookie=res.cookies
-object=json.loads(res.text)
-if object['code']==200:
-    print("登录成功！")
-else:
-    print("登录失败！请检查密码是否正确！"+str(object['code']))
-    exit(object['code'])
+        res = s.post(url=url,
+                     data=self.protect(json.dumps(logindata)),
+                     headers=headers2)
+        tempcookie = res.cookies
+        ans = json.loads(res.text)
+        if ans['code'] == 200:
+            self.log("> 登录成功！")
+        else:
+            self.log("> 登录失败！请检查密码是否正确！" + str(ans['code']), status_code=2)
+            self.self.send_msg(msg=self.LOG_INFO, SCK=self.SCK)
+            # exit(ans['code'])
 
-res=s.post(url=url2,data=protect('{"type":0}'),headers=headers)
-object=json.loads(res.text)
-if object['code']!=200 and object['code']!=-2:
-    print("签到时发生错误："+object['msg'])
-else:
-    if object['code']==200:
-        print("签到成功，经验+"+str(object['point']))
+        res = s.post(url=url2,
+                     data=self.protect('{"type":0}'),
+                     headers=headers)
+        ans = json.loads(res.text)
+        if ans['code'] != 200 and ans['code'] != -2:
+            self.log("> 签到时发生错误：" + ans['msg'], status_code=2)
+        else:
+            if ans['code'] == 200:
+                self.log("> 签到成功，经验+" + str(ans['point']))
+            else:
+                self.log("> 重复签到", status_code=1)
+
+        res = s.post(
+            url=url3,
+            data=self.protect(
+                '{"csrf_token":"' +
+                requests.utils.dict_from_cookiejar(tempcookie)['__csrf'] +
+                '"}'),
+            headers=headers)
+        ans = json.loads(res.text, strict=False)
+        for x in ans['recommend']:
+            url = 'https://music.163.com/weapi/v3/playlist/detail?csrf_token=' + requests.utils.dict_from_cookiejar(
+                tempcookie)['__csrf']
+            data = {
+                'id':
+                x['id'],
+                'n':
+                1000,
+                'csrf_token':
+                requests.utils.dict_from_cookiejar(tempcookie)['__csrf'],
+            }
+            res = s.post(url, self.protect(json.dumps(data)), headers=headers)
+            ans = json.loads(res.text, strict=False)
+            buffer = []
+            count = 0
+            for j in ans['playlist']['trackIds']:
+                data2 = {}
+                data2["action"] = "play"
+                data2["json"] = {}
+                data2["json"]["download"] = 0
+                data2["json"]["end"] = "playend"
+                data2["json"]["id"] = j["id"]
+                data2["json"]["sourceId"] = ""
+                data2["json"]["time"] = "240"
+                data2["json"]["type"] = "song"
+                data2["json"]["wifi"] = 0
+                buffer.append(data2)
+                count += 1
+                if count >= 310:
+                    break
+            if count >= 310:
+                break
+        url = "http://music.163.com/weapi/feedback/weblog"
+        postdata = {"logs": json.dumps(buffer)}
+        res = s.post(url, self.protect(json.dumps(postdata)))
+        ans = json.loads(res.text, strict=False)
+        if ans['code'] == 200:
+            log_info = "> 刷单成功！共" + str(count) + "首"
+            self.log(log_info)
+            # self.send_msg(msg=self.LOG_INFO, SCK=self.SCK)
+            # exit()
+        else:
+            log_info = "> 发生错误：" + str(ans['code']) + ans['message']
+            self.log(log_info, status_code=2)
+            # self.send_msg(msg=self.LOG_INFO, SCK=self.SCK)
+            exit(ans['code'])
+
+    def index(self):
+        """
+        程序入口
+        """
+        self.log("==")
+        self.log("##### 程序开始执行")
+        self.log("==")
+        self.deal_username_password()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        SCK = sys.argv[3]
+        AL = AutoLogin(username, password, SCK)
+        AL.index()
+    elif len(sys.argv) == 3:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        SCK = ""
+        AL = AutoLogin(username, password, SCK)
+        AL.index()
     else:
-        print("重复签到")
-
-
-res=s.post(url=url3,data=protect('{"csrf_token":"'+requests.utils.dict_from_cookiejar(tempcookie)['__csrf']+'"}'),headers=headers)
-object=json.loads(res.text,strict=False)
-for x in object['recommend']:
-    url='https://music.163.com/weapi/v3/playlist/detail?csrf_token='+requests.utils.dict_from_cookiejar(tempcookie)['__csrf']
-    data={
-        'id':x['id'],
-        'n':1000,
-        'csrf_token':requests.utils.dict_from_cookiejar(tempcookie)['__csrf'],
-    }
-    res=s.post(url,protect(json.dumps(data)),headers=headers)
-    object=json.loads(res.text,strict=False)
-    buffer=[]
-    count=0
-    for j in object['playlist']['trackIds']:
-        data2={}
-        data2["action"]="play"
-        data2["json"]={}
-        data2["json"]["download"]=0
-        data2["json"]["end"]="playend"
-        data2["json"]["id"]=j["id"]
-        data2["json"]["sourceId"]=""
-        data2["json"]["time"]="240"
-        data2["json"]["type"]="song"
-        data2["json"]["wifi"]=0
-        buffer.append(data2)
-        count+=1
-        if count>=310:
-            break
-    if count>=310:
-        break
-url = "http://music.163.com/weapi/feedback/weblog"
-postdata={
-    "logs":json.dumps(buffer)
-}
-res=s.post(url,protect(json.dumps(postdata)))
-object=json.loads(res.text,strict=False)
-if object['code']==200:
-    print("刷单成功！共"+str(count)+"首")
-    exit()
-else:
-    print("发生错误："+str(object['code'])+object['message'])
-    exit(object['code'])
+        AL = AutoLogin()
+        AL.log("""
+    错误： 参数传入错误！
+    使用方法: python checkin.py 手机号 密码 [server酱的SCK]
+    注： 手机号与密码可以为多个，请使用#隔开
+    例： python checkin.py 12344455#121321323 23123123#321312312 
+    注： server 酱可以将信息发送到微信，没有可以不填 
+        """,
+               status_code=2)
